53d0f537b942bd2737dd803dc2a947798a711e68
szzdmj <chuanfuyin001@gmail.com>
2025-07-27 22:38:45 +0800

    Update container.ts

--- src/container.ts
+++ src/container.ts
@@ -1,5 +1,5 @@
 export class MyContainer {
   async fetch(request: Request) {
-    return new Response("Hello from Durable Object container", { status: 200 });
+    return new Response("Hello from FrankenPHP container", { status: 200 });
   }
 }

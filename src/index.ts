90aa720e849cee3f577200ce64550a969444cd18
szzdmj <chuanfuyin001@gmail.com>
2025-07-27 21:48:40 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,13 @@ import { Container } from "@cloudflare/containers";
 
 export class MyContainer extends Container {
   async fetch(request: Request): Promise<Response> {
-    // 所有请求都转发给容器（Docker 镜像）
     return await this.run(request);
+  }
+}
+
+// 👇 显式导出 Durable Object 类（这是 ES Module Worker 关键）
+export default {
+  fetch(request: Request, env: Env, ctx: ExecutionContext) {
+    return new Response("FrankenPHP container is set up!");
   }
 }

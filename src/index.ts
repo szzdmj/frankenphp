0c885b79833276ec341170cc28b8977e0cffa38d
szzdmj <chuanfuyin001@gmail.com>
2025-07-29 00:41:57 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-import { MyContainer, handleContainerRequest } from "./container";
-import { createFactory } from "@cloudflare/containers";
 import { Hono } from "hono";
+import { createFactory } from "@cloudflare/containers";
+import { MyContainer, handleContainerRequest } from "./container";
 
 const app = new Hono();
 
-// 所有 /frankenphp/* 的請求將被轉發至 MyContainer 實例中
 app.use("/frankenphp/*", createFactory(MyContainer, handleContainerRequest));
 
 export default app;

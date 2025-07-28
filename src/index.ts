1258574955f79c42e5ac1213aad971659fa1ed42
szzdmj <chuanfuyin001@gmail.com>
2025-07-28 16:34:16 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
 import { MyContainer, handleContainerRequest } from "./container";
 
-// 必须导出 Durable Object 类名
 export { MyContainer };
 
 export default {
-  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    try {
-      return await handleContainerRequest(request, env, ctx);
-    } catch (err) {
-      console.error("Fatal error in fetch:", err);
-      return new Response("Internal Error", { status: 500 });
-    }
-  },
+  async fetch(request: Request, env: any, ctx: ExecutionContext) {
+    return handleContainerRequest(request, env);
+  }
 };

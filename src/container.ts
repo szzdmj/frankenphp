47c9f9610f1b1a91b04b6b93f713a82e2b7fc6e6
szzdmj <chuanfuyin001@gmail.com>
2025-07-28 20:10:49 +0800

    Update container.ts

--- src/container.ts
+++ src/container.ts
@@ -1,7 +1,7 @@
 import { Container } from "@cloudflare/containers";
 
 export class MyContainer extends Container {}
-
+const id = env.frankenphp_container.idFromName("main");
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     return await serve({

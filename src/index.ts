9080f361ed60bd21df87603a61eaee1dbc852087
szzdmj <chuanfuyin001@gmail.com>
2025-08-06 11:20:02 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-export { MyContainer } from "./container"; // 👈 必须添加：导出 Durable Object 类
+import { Container, getRandom } from "@cloudflare/containers";
+
+export class SZContainer extends Container {
+  defaultPort = 80; // Port the container is listening on
+  sleepAfter = "1m"; // Stop the instance if requests not sent for 1 minutes
+}
+
+const INSTANCE_COUNT = 5;
 
 export default {
-async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-const id = env.MY_CONTAINER.idFromName("default");
-const stub = env.MY_CONTAINER.get(id);
-return await stub.fetch(request);
-}
+  async fetch(request: Request, env: Env): Promise<Response> {
+  let container = await getRandom(env.SZ_CONTAINER, INSTANCE_COUNT);
+  // Pass the request to the container instance on its default port
+  return await container.fetch(request);
+  },
 };
-
-interface Env {
-MY_CONTAINER: DurableObjectNamespace;
-}
